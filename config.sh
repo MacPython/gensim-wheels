3cecaae7dfcf0c91c1c817d6d4f3b4480f166039
ivan <menshikh.iv@gmail.com>
2017-11-23 15:21:44 +0500

    moar test deps + preparation for wheel uploading

--- config.sh
+++ config.sh
@@ -11,5 +11,6 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c 'import gensim; print(gensim.__file__, gensim.models.word2vec.FAST_VERSION)'
-    pytest -rfxEXs --durations=20 --showlocals --rerun 3 --pyargs gensim -v  
+    pip freeze
+    pytest -rfxEXs --durations=20 --showlocals --rerun 3 --pyargs gensim
 }
