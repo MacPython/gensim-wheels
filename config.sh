7c5f8716e9476670e38c2b7e9e0e21a3c0a0bc6d
Ivan Menshikh <menshikh.iv@gmail.com>
2020-04-19 11:08:00 +0300

    [REVERT ME] Debug OSX 3.8[2]

--- config.sh
+++ config.sh
@@ -28,5 +28,9 @@ assert FV_W2V >= 0, FV_W2V
 assert FV_FT >= 0, FV_FT
 """
     pip freeze
+
+    python -W ignore -m unittest gensim.test.test_corpora || true
+    echo "=========================="
     pytest -rfxEXs --durations=20 --disable-warnings --showlocals --reruns 3 --reruns-delay 1 --pyargs gensim.test.test_corpora
+
 }
